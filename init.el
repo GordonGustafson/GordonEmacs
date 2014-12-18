9966bb1b31bd843b3c436163c82682350bccb9d7
Gordon Gustafson <gordon3.14@gmail.com>
2014-12-17 22:48:12 -0500

    Don't update all packages on startup
    First of all package-desc-vers was removed in Emacs 24.4 and
    package-desc-version doesn't seem to work.
    
    Second of all, updating all packages on startup is unnecessary.
    To update all packages, do M-x list-packages followed by "U x".
    This *drastically* improves startup time and is much more reliable.
    I highly doubt running two-week only packages will hurt me.
    
    To set up Emacs on a new machine, do M-x install-gordon-packages.

--- init.el
+++ init.el
@@ -140,7 +140,6 @@ override all others.")
 
 
 ;; settings loaded last because they could cause problems
-(require 'cl)
 (require 'package)
 (package-initialize)
 
@@ -148,20 +147,6 @@ override all others.")
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 
-(defun package-installed-and-up-to-date-p (package)
-  ;; assumes that package-refresh-contents has already been called
-  ;; also assume all built-in packages are out of date, which is usually true.
-  (if (not (package-installed-p package))
-      nil
-    (let* ((newest-desc  (cdr (assq package package-archive-contents)))
-           (installed-desc (cdr (or (assq package package-alist) (assq package package--builtins))))
-           (newest-version  (package-desc-vers newest-desc))
-           (installed-version (package-desc-vers installed-desc)))
-      (or (equal installed-version newest-version) (version-list-< newest-version installed-version)))))
-
-(message "%s" "Emacs Prelude is now refreshing its package database...")
-(package-refresh-contents)
-(message "%s" " done.")
 
 (defvar gordon-packages '(color-theme
                           org auctex frame-cmds frame-fns
@@ -170,12 +155,10 @@ override all others.")
                           ggtags goto-chg undo-tree find-file-in-project)
   "Packages that will be installed/updated to the latest version on startup")
 
-(dolist (package gordon-packages)
-  (when (not (package-installed-and-up-to-date-p package))
-    (package-install package)))
+(defun install-gordon-packages ()
+  (interactive)
+  (mapc 'package-install gordon-packages))
 
-(dolist (upgradeable-packgage (package-menu--find-upgrades))
-  (package-install upgradeable-packgage))
 
 (require 'color-theme)
 (color-theme-initialize)
