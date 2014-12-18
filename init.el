b0a32032a898ad5cb60b2392ff029f027d6b77a0
Gordon Gustafson <gordon3.14@gmail.com>
2014-12-17 23:02:10 -0500

    Remove unused bash-completion from gordon-packages
    It was never loaded or used in any way.

--- init.el
+++ init.el
@@ -150,7 +150,7 @@ override all others.")
 
 (defvar gordon-packages '(color-theme
                           org auctex frame-cmds frame-fns
-                          bash-completion csharp-mode openwith git-commit-mode
+                          csharp-mode openwith git-commit-mode
                           magit auto-complete smex rainbow-delimiters dsvn
                           ggtags goto-chg undo-tree find-file-in-project)
   "Packages that will be installed/updated to the latest version on startup")
