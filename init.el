e0a9a0b1e70f9aff3d84954ab1d876c419609a3f
Gordon Gustafson <gordon.gustafson@hulu.com>
2016-08-11 11:51:21 -0700

    set-fringe-style -> set-fringe-mode

--- init.el
+++ init.el
@@ -62,7 +62,8 @@ override all others.")
 
 (set-face-attribute 'default nil :family "Consolas" :height 105)
 ;; Remove fringes on windows to avoid wasting space
-(set-fringe-style 0)
+(set-fringe-mode 0)
+
 ;; Seting these to newlines makes one extra character fit on each line.
 ;; I don't know why this works; it may break in the future.
 (let ((newline-glyph (make-glyph-code ?\n)))
